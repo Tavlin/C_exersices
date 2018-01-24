1fe8251e2edafb67f0600e8d7855efc113349773
Luke Bovard <lukebovard@gmail.com>
2018-01-24 10:48:35 +0100

    the error was that vector was just a pointer ot value and you cannot reallocate the pointer vector whenit just points to value

--- 11/11.c
+++ 11/11.c
@@ -2,94 +2,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-
 // struct says: "DON'T ASSSUME MY SIZE"
-
 // capacity == max number of elements that fit in the allocated storage
 // size == #elements stored in the vector
 long int capacity, size;
 double* vector;
 
-void createVector(const unsigned int numberOfElements, /*const*/ double* value)
-{
-	// (a) set capacity to the smallest power of 2 larger than the provided 
-	// numberOfElements
-	
+void createVector(const unsigned int numberOfElements, /*const*/ double* value) {
 	long int x = 2;
-	while(x < numberOfElements)
-	{
+	while(x < numberOfElements) {
 		x *= 2;  
-	}
-	
-	capacity = x;
-	
-	// (b) make a first memory allocation of capacity*sizeof(/*type*/) bytes,
-	// using the malloc function;
-	
-	vector = (double*)malloc(capacity * sizeof(double));
-	
-	// (c) set the size of our vector to numberOfElements and initialize the first 
-	// elements of vector to value.
-	
+	} 
+	capacity = x; 
+	vector = (double*)malloc(capacity * sizeof(double)); 
 	size = numberOfElements;
-	vector = value;
-	
+  for(int i=0;i<numberOfElements;i++) {
+    vector[i] = value[i]; 
+  }
 }
 
-void destroyVector(void)
-{
-	free(vector);
-	size = capacity = 0;
-	vector = NULL;
+void destroyVector(void) {
+	free(vector); size = capacity = 0; vector = NULL;
 }
 
-// retrieve content with boundary check
-double at(const unsigned int index)
-{
-	if(index >= size)
-	{
-		printf("index is bigger then vector size");
+double at(const unsigned int index) {
+	if(index >= size) {
+		printf("index is bigger then vector size\n");
 		exit(0);
-	}
-	
+	} 
 	return vector[index];
 }
 
-// to add an element at the end
-void pushBack(const double value)
-{	
-	
-	size += 1;
-	
-	if(capacity <= size)
-	{
-		capacity *= 2;
-		printf("size = %ld\t capacity = %ld\n", size, capacity);
-	
-	double* new_vector;
-	new_vector = (double *)realloc(vector, capacity*sizeof(double));
-		if(new_vector == NULL)
-		{
-			printf("error: realloc gone wrong!\n");
-			exit(0);
-		}
-		vector = new_vector;
-	}
-	//free(new_vector);	
-
-	//vector[size-1] = value;
-	
+void pushBack(const double value) {	
+  size += 1; 
+  if(capacity <= size) {
+    capacity *= 2;
+    printf("inside push back size = %ld\t capacity = %ld\n", size, capacity); 
+    double* new_vector = (double *)realloc(vector, capacity*sizeof(double));
+    printf("you'll never see me!\n");
+  }
 }
 
-//to remove an element at the end
-void popBack(void)
-{	
-	
-	//vector[size-1] = NULL; // NULL type is void, values of pointer are double :c
-	//free(&vector[size-1]);
-	size -= 1;
-	
+void popBack(void) {	
+	size -= 1; 
 	while((capacity/2) > size)
 	{
 		capacity /= 2;
@@ -104,11 +59,9 @@ void popBack(void)
 	vector = (double *)realloc((void *)vector,(capacity)*sizeof(double));
 }
 
-// print function
-void print_vector(double * vector)
-{
-	for(int i = 0; i < size; i++)
-	{
+void print_vector(double * vector) {
+  printf("size = %ld \t capacity = %ld\n",size,capacity);
+	for(int i = 0; i < size; i++) {
 		printf("vector[%d] = %lf\n", i, vector[i]);
 	}
 }
@@ -116,17 +69,19 @@ void print_vector(double * vector)
 int main(int argc, char *argv[])
 {
 	double start_vector[argc];
-	for(int i = 0; i < argc ; i++)
-	{
-		start_vector[i] = atoi(argv[i]);
-	}
-	printf("peep\n");
-	createVector(12, start_vector);
+//	for(int i = 0; i < argc ; i++) {
+//		start_vector[i] = atoi(argv[i]);
+    
+  for(int i = 0; i < 3 ; i++) {
+    start_vector[i] = 3-i;
+}
+	createVector(3, start_vector);
 	print_vector(vector);
 	
-	const unsigned int asdf = 3;
-	printf("using at %d: %lf\n", asdf, at(asdf));
+//	const unsigned int asdf = 3;
+//	printf("using at %d: %lf\n", asdf, at(asdf));
 	
+	pushBack(14);
 	/*
 	pushBack(14);
 	pushBack(14);
@@ -138,10 +93,9 @@ int main(int argc, char *argv[])
 	pushBack(14);
 	pushBack(14);
 	pushBack(14);
-	pushBack(14);
 	print_vector(vector);*/
-	popBack();
-	print_vector(vector);
+//	popBack();
+//	print_vector(vector);
 	
 	return 0;
 }
