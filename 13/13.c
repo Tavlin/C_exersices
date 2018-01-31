d364cdd5a2092716495fbbb5c734df666f922f6e
Programmierpraktikum WS1718 <pp-ws1718-011@users.itp.uni-frankfurt.de>
2018-01-31 11:44:04 +0100

    update

--- 13/13.c
+++ 13/13.c
@@ -2,6 +2,35 @@
 #include <stdlib.h>
 #include <math.h>
 
+typedef struct PartialSum{
+	int p;
+	int lower_bound;
+	int upper_bound;
+	int delta;
+}PS;
+
+FILE* file;
+
+void readInputFile(/*...*/)
+{
+	int ir;
+	file = fopen("input.dat", "r");
+	
+	while(1)
+	{
+		ir = fscanf(file, "%d", &ir);
+		if(ir == EOF)
+		{
+			break;
+		}
+	}
+}
+
+double getPartialSum(/*...*/)
+{
+	
+}
+
 int main(void)
 {
 	
